f617b3dfa265a354cdd699ff467769368b3e1333
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2022-03-10 14:35:45 -0600

    commit tree 936eeac47b8778ea069780357bfd27978c6edbc5

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    if (pickup > 42) {
+return pickup - 42
+    } return 42 - pickup
+}
+distanceFromHqInBlocks();
+
+function distanceFromHqInFeet(blocks) {
+  return distanceFromHqInBlocks(blocks) * 264
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start > destination) {
+        return (start - destination) * 264
+    } return (destination - start) * 264
+}
+function calculatesFarePrice(start, destination) {
+   let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+        return 0
+    } else if (distance >= 400 && distance < 2000) {
+        return (distance - 400) * .02 
+    } else if(distance >= 2000 && distance < 2500) {
+        return 25
+    } else if(distance > 2500) {
+         return "cannot travel that far"
+    }
+}
